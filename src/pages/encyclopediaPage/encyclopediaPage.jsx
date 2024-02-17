802a2f1d71968a385e04526c29153051b2c6662b
tenoven <tenoven@outlook.com>
2024-02-17 22:37:10 +0100

    details added

--- src/pages/encyclopediaPage/encyclopediaPage.jsx
+++ src/pages/encyclopediaPage/encyclopediaPage.jsx
@@ -20,12 +20,12 @@ function EncyclopediaPage(props) {
                     <h1>Encyclopedia</h1>
                     <article className="encyclopediaBody">
                         <EncyclopediaTile title="Races" image={raceimg} imageAlt="image of people in a circle" linkPage=""/>
-                        <EncyclopediaTile title="Classes" image={classesimg} imageAlt="" linkPage=""/>
-                        <EncyclopediaTile title="Backgrounds" image={backgroundimg} imageAlt="" linkPage="" />
-                        <EncyclopediaTile title="Feats" image={featsimg} imageAlt="" linkPage="" />
-                        <EncyclopediaTile title="Items" image={itemimg} imageAlt="" linkPage="" />
-                        <EncyclopediaTile title="Spells" image={spellsimg} imageAlt="" linkPage="" />
-                        <EncyclopediaTile title="Monsters" image={monsterimg} imageAlt="" linkPage="" />
+                        <EncyclopediaTile title="Classes" image={classesimg} imageAlt="image of a wizards hat" linkPage=""/>
+                        <EncyclopediaTile title="Backgrounds" image={backgroundimg} imageAlt="image of a piece of paper" linkPage="" />
+                        <EncyclopediaTile title="Feats" image={featsimg} imageAlt="image of a diamond" linkPage="" />
+                        <EncyclopediaTile title="Items" image={itemimg} imageAlt="image of a mushroom and a twig" linkPage="" />
+                        <EncyclopediaTile title="Spells" image={spellsimg} imageAlt="image of a spellbook" linkPage="" />
+                        <EncyclopediaTile title="Monsters" image={monsterimg} imageAlt="image of a monster with multiple eyes" linkPage="" />
                     </article>
                 </main>
             </BasicDragonBackground>
