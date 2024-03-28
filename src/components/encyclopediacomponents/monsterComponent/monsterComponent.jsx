a8c2a148d4c197ea7ab7bb4249d32c109166877b
tenoven <tenoven@outlook.com>
2024-03-28 12:43:16 +0100

    monsterpage done

---
+++ src/components/encyclopediacomponents/monsterComponent/monsterComponent.jsx
@@ -0,0 +1,146 @@
+import {useState} from "react";
+import Button from "../../buttons/button.jsx";
+import "./monsterComponent.css"
+
+function MonsterComponent(props) {
+
+    const [showMore, setShowMore] = useState(false);
+    const apiData = props.data
+
+    return (
+        <div className="monsterCard">
+            <fieldset className="boxes">
+                <h2>{apiData.name}</h2>
+                <p>{apiData.size +" "+ apiData.type +" , "+ apiData.alignment}</p>
+
+
+                {showMore ? (
+                    <>
+                        <section>
+                            {apiData.img_main != null ? (
+                                <img src={apiData.img_main} alt=""/>
+                            ) : null }
+                        </section>
+
+
+                        <section className="info_row">
+                            <p>Armor class: {apiData.armor_class} ~ </p>
+                            <p>Hit points: {apiData.hit_points} ({apiData.hit_dice}) ~ </p>
+                            <p> <>Speed: </>
+                                {Object.entries(apiData.speed).map(([key, value], index) => (
+                                    <span key={index}>
+
+                                        {key}: {value}
+                                        {index !== Object.keys(apiData.speed).length - 1 && ", "}
+                                    </span>
+                                ))}
+                                <> ~ </>
+                            </p>
+                            <p>CR: {apiData.cr}</p>
+                        </section>
+
+                        <section className="info_row">
+                            <p>Str: {apiData.strength} ~</p>
+                            <p>Dex: {apiData.dexterity} ~</p>
+                            <p>Con: {apiData.constitution} ~</p>
+                            <p>Int: {apiData.intelligence} ~</p>
+                            <p>Wis: {apiData.wisdom} ~</p>
+                            <p>Cha: {apiData.charisma}</p>
+                        </section>
+
+
+                        {apiData.strength_save != null && apiData.dexterity_save != null && apiData.constitution_save != null && apiData.intelligence_save != null && apiData.wisdom_save != null && apiData.charisma_save != null && (
+                            <section className="info_row">
+                                {apiData.strength_save != null ? (
+                                    <p>Strength save: {apiData.strength_save}</p>
+                                ) : null }
+
+                                {apiData.dexterity_save != null ? (
+                                    <p>Dex save: {apiData.dexterity_save}</p>
+                                ) : null }
+
+                                {apiData.constitution_save != null ? (
+                                    <p>Con save: {apiData.constitution_save}</p>
+                                ) : null }
+
+                                {apiData.intelligence_save != null ? (
+                                    <p>Int save: {apiData.intelligence_save}</p>
+                                ) : null }
+
+                                {apiData.wisdom_save != null ? (
+                                    <p>Wis save: {apiData.wisdom_save}</p>
+                                ) : null }
+
+                                {apiData.charisma_save != null ? (
+                                    <p>Cha save: {apiData.charisma_save}</p>
+                                ) : null }
+                            </section>
+                        )}
+
+
+                        <section className="info_column">
+                            {apiData.skills != null ? (
+                            <p><>Skills: </>
+                            {Object.entries(apiData.skills).map(([key, value], index) => (
+                                <span key={index}>
+                                        {key}: {value}
+                                    {index !== Object.keys(apiData.speed).length - 1 && ", "}
+                                    </span>
+                            ))} </p>
+                            ) : null }
+                            <p>Senses: {apiData.senses}</p>
+                            <p>Languages: {apiData.languages}</p>
+                        </section>
+
+                        {apiData.special_abilities != null ? (
+                            <section className="info_column">
+                                {apiData.special_abilities.map((dat, index) => (
+                                    <div key={index}>
+                                        <p><b>{dat.name}:</b></p>
+                                        <p>{dat.desc}</p>
+                                    </div>
+                                ))}
+                            </section>
+                        ) : null }
+
+
+
+                        <section className="info_column">
+                            <h3>Actions:</h3>
+                            {apiData.actions.map((dat, index) => (
+                                <div key={index}>
+                                    <p><b>{dat.name}:</b></p>
+                                    <p>{dat.desc}</p>
+                                </div>
+                            ))}
+                        </section>
+
+                        {apiData.legendary_actions != null ? (
+                            <section>
+                                    <>
+                                        <h3>Legendary actions:</h3>
+                                        <p><i>{apiData.legendary_desc}</i></p>
+                                        {apiData.legendary_actions.map((dat, index) => (
+                                            <div key={index}>
+                                                <p><b>{dat.name}:</b></p>
+                                                <p>{dat.desc}</p>
+                                            </div>
+                                        ))}
+                                    </>
+                            </section>
+                            ) : null }
+
+
+                        <Button className="yellow" onClick={() => setShowMore(false)}>Show less</Button>
+                    </>
+                ) : (
+                    <>
+                        <Button className="yellow" onClick={() => setShowMore(true)}>Show more</Button>
+                    </>
+                )}
+            </fieldset>
+        </div>
+    );
+}
+
+export default MonsterComponent;
--- src/pages/encyclopediaPage/monsters/monsterPage.jsx
+++ src/pages/encyclopediaPage/monsters/monsterPage.jsx
@@ -1,15 +1,19 @@
 import {useEffect, useState} from 'react';
 import axios from "axios";
 import BasicDragonBackground from "../../../components/backgrounds/basicDragon/basicDragonBackground.jsx";
+import MonsterComponent from "../../../components/encyclopediacomponents/monsterComponent/monsterComponent.jsx";
+import Button from "../../../components/buttons/button.jsx";
+import "./monsterPage.css"
 
 function MonsterPage() {
 
     const [apiData, setApiData] = useState([])
+    const [apiLink, setApiLink] = useState("https://api.open5e.com/v1/monsters/?format=json")
 
     useEffect(() => {
         async function apiGetInfo() {
             try {
-                const response = await axios.get("https://api.open5e.com/v1/monsters/?format=json");
+                const response = await axios.get(apiLink);
                 console.log(response.data)
                 setApiData(response.data)
             } catch (error) {
@@ -17,15 +21,55 @@ function MonsterPage() {
             }
         }
         void apiGetInfo()
-    }, [])
+    }, [apiLink])
+
+    function setNext() {
+        setApiLink(apiData.next)
+    }
+
+    function setPrev() {
+        setApiLink(apiData.previous)
+    }
 
     return (
         <BasicDragonBackground>
-            <main>
-                <p>monsterpage</p>
+            <main className="monster-container">
+                <h1>Monster Bestiary</h1>
+
+                <div className="prevNextButtons">
+                    {apiData.previous !== null && (
+                        <Button onClick={setPrev} className="yellow" >Previous</Button>
+                    )}
+
+                    {apiData.next !== null && (
+                        <Button onClick={setNext} className="yellow" >Next</Button>
+                    )}
+                </div>
+
+                <div className="background-container">
+                    {apiData.count > 0 ? (
+
+                        apiData.results.map((dat, index) => (
+                            <MonsterComponent key={index} data={dat}/>
+                        ))
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+                </div>
+
+                <div className="prevNextButtons">
+                    {apiData.previous !== null && (
+                        <Button onClick={setPrev} className="yellow" >Previous</Button>
+                    )}
+
+                    {apiData.next !== null && (
+                        <Button onClick={setNext} className="yellow" >Next</Button>
+                    )}
+                </div>
             </main>
         </BasicDragonBackground>
     );
 }
 
 export default MonsterPage;
+
