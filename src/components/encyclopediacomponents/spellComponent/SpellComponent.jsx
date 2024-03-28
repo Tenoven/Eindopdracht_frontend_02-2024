8a80b1956c88e999c5eb22eed2b2a919ccd771e1
tenoven <tenoven@outlook.com>
2024-03-28 10:03:42 +0100

    finished the spell pages

---
+++ src/components/encyclopediacomponents/spellComponent/SpellComponent.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import Button from "../../buttons/button.jsx";
+
+function SpellComponent(props) {
+    const [showMore, setShowMore] = useState(false);
+    const apiData = props.data
+
+    return (
+        <div className="featCard">
+            <fieldset className="boxes">
+                <h2>{apiData.name}</h2>
+
+                {showMore ? (
+                    <>
+                        <p><i>Spell level: {apiData.level}</i></p>
+                        <p><b>Casting time:</b> {apiData.casting_time}</p>
+                        <p><b>Range:</b> {apiData.range}</p>
+                        <p><b>Components:</b> {apiData.components} <i>{apiData.material !== "" && (<span>"{apiData.material}"</span>)}</i></p>
+                        <p><b>Duration:</b> {apiData.duration}</p>
+                        <p><b>Concentration:</b> {apiData.concentration}</p>
+                        <p>{apiData.desc}</p>
+                        
+                        <Button className="yellow" onClick={() => setShowMore(false)}>Show less</Button>
+                    </>
+                ) : (
+                    <Button className="yellow"  onClick={() => setShowMore(true)}>Show more</Button>
+                )}
+
+            </fieldset>
+        </div>
+    );
+}
+
+export default SpellComponent;
--- src/pages/encyclopediaPage/spells/spellPage.jsx
+++ src/pages/encyclopediaPage/spells/spellPage.jsx
@@ -1,32 +1,70 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import axios from "axios";
 import BasicDragonBackground from "../../../components/backgrounds/basicDragon/basicDragonBackground.jsx";
 import Button from "../../../components/buttons/button.jsx";
+import SpellComponent from "../../../components/encyclopediacomponents/spellComponent/SpellComponent.jsx";
+import "./spellPage.css"
 
 function SpellPage() {
-    let apiData = {}
+
+    const [apiData, setApiData] = useState({})
+    const [apiLink, setApiLink] = useState("https://api.open5e.com/v1/spells/?format=json")
 
     useEffect(() => {
         async function apiGetInfo() {
             try {
-                const response = await axios.get("https://api.open5e.com/v1/spells/?format=json");
-                console.log(response)
-                apiData = response
+                const response = await axios.get(apiLink);
+                console.log("on mount:", response.data)
+                setApiData(response.data)
             } catch (error) {
                 console.error('Error:', error);
             }
         }
         void apiGetInfo()
-    }, [])
+    }, [apiLink])
+
+    function setNext() {
+        setApiLink(apiData.next)
+    }
+
+    function setPrev() {
+        setApiLink(apiData.previous)
+    }
 
     return (
         <BasicDragonBackground>
-            <main>
-                <p>spells page</p>
-                <Button type="button" onClick={() => {
-                    console.log(apiData)}}>
-                    test
-                </Button>
+            <main className="spellPage">
+                <h1>Spells</h1>
+                <div className="prevNextButtons">
+                    {apiData.previous !== null && (
+                        <Button onClick={setPrev} className="yellow" >Previous</Button>
+                    )}
+
+                    {apiData.next !== null && (
+                        <Button onClick={setNext} className="yellow" >Next</Button>
+                    )}
+                </div>
+
+                <div className="spell-container">
+                    {console.log("apidata in return", apiData)}
+                    {apiData.count > 0 ? (
+                        apiData.results.map((dat, index) => (
+                            <SpellComponent key={index} data={dat}/>
+                        ))
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+                </div>
+
+                <div className="prevNextButtons">
+                    {apiData.previous !== null && (
+                        <Button onClick={setPrev} className="yellow" >Previous</Button>
+                    )}
+
+                    {apiData.next !== null && (
+                        <Button onClick={setNext} className="yellow" >Next</Button>
+                    )}
+                </div>
             </main>
         </BasicDragonBackground>
     );
