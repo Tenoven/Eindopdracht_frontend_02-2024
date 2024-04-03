57cbb6c5cb1b9c6a58a1d938735ff56ed485329d
tenoven <tenoven@outlook.com>
2024-04-03 10:01:07 +0200

    klein werk aan login pagina

--- src/pages/loginPage/loginPage.jsx
+++ src/pages/loginPage/loginPage.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import BasicDragonBackground from "../../components/backgrounds/basicDragon/basicDragonBackground.jsx";
 import "./loginPage.css"
+import Button from "../../components/buttons/button.jsx";
 
 function LoginPage(props) {
     return (
         <>
             <BasicDragonBackground>
-            <p>login</p>
+                <div>
+                    <h1>Login</h1>
+                    <p>welcome! Please log in</p>
+                </div>
+
+                {/*<form onSubmit={}>*/}
+                <form>
+                    <div>
+                        <label htmlFor="">Email:</label>
+                        <input type="email"/>
+                    </div>
+                    
+                    <div>
+                        <label htmlFor="">Password:</label>
+                        <input type="password"/>
+                    </div>
+                    <Button type="submit" className="yellow" >Submit login</Button>
+                </form>
             </BasicDragonBackground>
         </>
     );
