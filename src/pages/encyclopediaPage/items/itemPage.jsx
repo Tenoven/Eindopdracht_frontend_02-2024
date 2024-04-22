e255fca3391b79cdc027d8956b0f353ceea88e11
tenoven <tenoven@outlook.com>
2024-04-22 17:22:23 +0200

    small styling changes

--- src/pages/encyclopediaPage/items/itemPage.jsx
+++ src/pages/encyclopediaPage/items/itemPage.jsx
@@ -1,6 +1,9 @@
 import BasicDragonBackground from "../../../components/backgrounds/basicDragon/basicDragonBackground.jsx";
 import EncyclopediaTile from "../../../components/encyclopediacomponents/encyclopediaComponent/encyclopediaTile.jsx";
-import magicItemIMG from "../../../assets/subclasses.png"
+import magicItemIMG from "../../../assets/magic Item.png"
+import weaponIMG from "../../../assets/Weapons.png"
+import armorIMG from "../../../assets/Armor.png"
+
 import "./itemPage.css"
 
 function ItemPage() {
@@ -12,9 +15,8 @@ function ItemPage() {
                     <p>Choose the type of item you want to find information about:</p>
                     <article className="tileBase">
                     <EncyclopediaTile title="Magic items" image={magicItemIMG} imageAlt="a hand holding a wand" linkPage="/encyclopedia/items/magicitems"/>
-                    <EncyclopediaTile title="Weapons" image={magicItemIMG} linkPage="/encyclopedia/items/weapons"/>
-                    <EncyclopediaTile title="Armor" image={magicItemIMG} linkPage="/encyclopedia/items/armor"/>
-                    {/*<EncyclopediaTile title=""/>*/}
+                    <EncyclopediaTile title="Weapons" image={weaponIMG} linkPage="/encyclopedia/items/weapons"/>
+                    <EncyclopediaTile title="Armor" image={armorIMG} linkPage="/encyclopedia/items/armor"/>
                     </article>
                 </main>
             </BasicDragonBackground>
